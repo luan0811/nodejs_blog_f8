60c7ef6d9dd12fe9babe737f60da4db2143c6bec
luan0811 <luanntmse173311@fpt.edu.vn>
2024-05-08 11:39:51 +0700

    test

--- src/index.js
+++ src/index.js
@@ -23,8 +23,8 @@ app.engine(
     extname: '.hbs',
   }),
 );
-        app.set('view engine', 'hbs');
-        app.set('views', path.join(__dirname, 'resources/views'));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 
 // đang start ra 1 cái port webserver
